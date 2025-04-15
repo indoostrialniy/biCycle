853c4a6156f26226737e8af9efe7c0adc0feef63
Pavel <pawel.iv.2016@yandex.ru>
2025-04-15 09:13:41 +0300

    biCycle v1.5
    Some improvements

--- biCycle.h
+++ biCycle.h
@@ -2,196 +2,102 @@
 #ifndef BI_CYCLE_H
 #define BI_CYCLE_H
 
-
 #include <iostream>
 #include <vector>
 #include <functional>
-//#include "nameof.hpp" // подключить при необходимости ( https://github.com/Neargye/nameof )
 
-#define BICYCLE_LIBRARY
+#define BI_CYCLE_ENABLED
 
+#warning "Ensure that you have added Execute()-call in main loop!"
 
-/*
-	2025-03-04  indoostrialniy  <pawel.iv.2016@yandex.ru>
-	
-	Описание библиотеки biCycle v1.4
-	Называется велосипедом не просто так ¯\_(ツ)_/¯
-	
-		Назначение:
-	
-	Заголовочный файл "biCycle.h" описывает класс biCycleSequencer, который позволяет "запоминать" функции, отвечающие некоторым требованиям и
-	описывающие пользовательские скриптовки, и обращаться к ним в каждом цикле пользовательской программы до тех пор, пока они не вернут true.
-	В качестве аргумента в функцию-метод передается некий пользовательский объект.
-	Имеет смысл в качестве типа описывать структуру даннных, в которую, например, по желанию можно добавлять булев флаг о завершении или вспомогательную int-переменную (см. Способ применения п.4).
-	Кастомная структура данных позволит при дальнейшем масштабировании программы не перелопачивать весь код, добавляя аргументы. Своего рода - задел на будущее.
-	 
-	Ко всем обозначениям добавлена приставка biCycle для минимизации вероятности конфликта имен в проекте, использующем множество библиотек.
-	 
-	Способ применения:
-	1)	Подключить данный заголовочный файл:
-		#include "biCycle.h"
-	
-	2)	В начале программы описать тип кастомного аргумента (yourType) и ОБЯЗАТЕЛЬНО объявить #define BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE на ваш тип:
-		#define BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE yourType
-	
-	2)	После подключения создать экземпляр класса biCycleSequencer с удобным именем, в данном примере - "Flow", с инициализацией шаблона пользовательским типом передаваемого в хранимую функцию аргумента:
-		biCycleSequencer<BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE> Flow;
-	
-	3)	В нужном пользователю месте, вероятнее всего - в главной петле, прописать вызов метода Execute():
-		
-		void mainLoop()
-		{
-			while (!glfwWindowShouldClose(window))
-			{
-				Flow.Execute();
-			
-				glfwPollEvents();
-				glfwSwapBuffers(window);
-			}
-		}
-		Этих 3 шагов достаточно для интеграции Секвенсора в проект.
-	
-	4)	Подмешивание последовательностей в основной поток программы происходит с помощью вызова метода CallSequence(), 
-			в аргумент которого передается структура biCycleSequenceWrapper обертки подмешиваемой функции либо
-			в аргумент записываются сразу компоненты обертки через фигурные скобки. (См.описание обертки). Возможные варианты:
-			
-			// способ 1
-			std::function<bool(BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE)> startFunction = std::bind( &Body::testFunc, body);
-			biCycleSequenceData datas(&completeFlag, auxVar);
-			Flow.CallSequence( { std::move(startFunction), std::move(datas) } );								
-			
-			// способ 2
-			Flow.CallSequence( { std::bind( &Body::testFunc, body), biCycleSequenceData(&completeFlag, auxVar) } );	
-			
-			// способ 3
-			Flow.CallSequence( { std::bind( &Body::testFunc, body), biCycleSequenceData() } );	
-			
-			// способ 4
-			Flow.CallSequence( { std::bind( &Body::testFunc, body) } );
-	
-	
-	Описание работы:
-	Для реализации своего назначения библиотека содержит:
-	1) необходимую для правильной работы обертку biCycleSequenceWrapper функции, чье выполнение мы хотим подмешать в основной цикл программы
-	
-	3) класс biCycleSequencer, содержащий:	
-		3.1) вектор sequences, хранящий все приписанные обертки
-		3.2) метод CallSequence(), позволяющий добавить последовательность в вектор исполняемых sequences
-		3.3) метод Execute(), чей вызов ОБЯЗАТЕЛЬНО надо вызывать в определенном пользователем месте основного потока программы. 
-			Это единственный ОБЯЗАТЕЛЬНЫЙ "мостик" между Секвенсором и основной программой
-
-	Правильно занесенная в Секвенсор последовательность будет исполняться до тех пор, пока не вернет true.
-	Если не прописать возврат true, подмешанная последовательность будет исполняться вплоть до закрытия программы.
-	Т.о. можно удобно реализовывать некоторые скрипты по принципу "вызвал исполнение - и забил". Например, прописать т.о. действия, выполнение которых не требуется в каждом цикле, а лишь с определенным интервалом.
- 
-	Терминология: 
-	последовательность 	- это функция, описывающая набор каких-либо временнО- или цикло-ориентированных событий 
-	Секвенсор 			- класс, содержащий всё необходимое для работы механизма подмешивания последовательности в основной цикл программы
-
-
-	Функция, которую собираемся подмешать в поток команд цикла, имеет 4 аргумента - структуру со вспомогательными параметрами,  также "коллбек"-функции начала и конца,
-	и должна возвращать булеву переменную. Такова задумка.
-
-
-	Шаблон, которого должна придерживаться пользовательская последовательность (функция/метод).
-	
-	Основные требования: "шапка"
-			bool yourOwnSequenceName(BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE& data)
-	и, соответственно, обязательное возвращение bool в теле функции.
-	
-	Возвращенный true прекращает дальнейшее исполнение последовательности (смотри функцию Execute() )
-	
-
-	Обертка:
-	
-	Структура SequenceWrapper - это "обертка" для подмешиваемой последовательности, она хранит: 
-	адрес функции; 
-	структуру с данными аргументов ( указатель на флаг завершенности функции (использующийся для сигнализации о выполнении последовательности), вспомогательная целочисленная переменная-аргумент и т.д.
-*/
-
-
-// Структура хранит необходимые данные для корректного занесения последовательности в секвенсор
-template <typename S>		
-struct biCycleSequenceWrapper
+namespace biCycle
 {	
-	std::function<bool(S&)> 	function;
-	S							data ;
+	/*
+		2025-04-15  indoostrialniy  <pawel.iv.2016@yandex.ru>
+		
+		biCycle library v1.5 description
+		Называется велосипедом не просто так ¯\_(ツ)_/¯
+	*/
+	template <typename DataPack>							// DataPack - type of structure with data, which will be pushed as argument into "function" via Execute()
+	struct Wrapper											// mixed func Wrapper
+	{	
+		std::function<bool(DataPack&)> 	function;			// returned bool signalize about completing
+		DataPack						data;
+		
+		std::function<void()>			start_callback;		// empty callback
+		std::function<void()>			end_callback;
+		
+		std::function<bool(DataPack&)>	startFunc;			// data callback
+		std::function<bool(DataPack&)>	endFunc;
+	};
+
 	
-	std::function<bool(S&)>		startFunc;								// с аргументами
-	std::function<bool(S&)>		endFunc;								
 	
-	std::function<void()>		start_callback;							// без аргументов
-	std::function<void()>		end_callback;
-};
-
-
-
-template <typename S>
-class biCycleSequencer
-{
-	std::vector <biCycleSequenceWrapper<S> > sequences;					//вектор с обертками функций
-
-public:
-	// функция CallSequence() лишь принимает ссылочную оберточную структуру biCycleSequenceWrapper функции и сохраняет ее в приватном векторе sequences
-	void CallSequence( const biCycleSequenceWrapper<S>& newSequence)
+	template <typename DataPack>	
+	class Sequencer
 	{
-		sequences.push_back( std::move(newSequence) );	
+		std::vector<Wrapper<DataPack>> 	sequences;					// func-shell-vector
+	public:
+		~Sequencer()				{	sequences.clear(); 	}		// auto clearing when exit
 		
-		// так как вносим в конец вектора новую последовательность, которую подразумевается сразу же начать исполнять, то для вызова начального колбека
-		// получим последний элемент вектора функцией .back(), которая и будет нашей вносимой
-		
-		if(sequences.back().startFunc)
-		{	sequences.back().startFunc(sequences.back().data);	}		// вызов стартовой функции!!!
-		
-		if(sequences.back().start_callback)
-		{ sequences.back().start_callback();	}
-		
-	}
-
-	// функция Execute() должна вызываться из основной части программы. Например, можно вызывать ее в каждом цикле главной петли
-	void Execute()
-	{
-		if( !sequences.empty() )
+		void CallSequence( const Wrapper<DataPack>& newSequence)	// функция CallSequence() лишь принимает ссылочную оберточную структуру Wrapper функции и сохраняет ее в приватном векторе sequences
 		{
-			auto it = sequences.begin();
+			sequences.push_back( std::move(newSequence) );	
 			
-			while (it != sequences.end()) 
+			// так как вносим в конец вектора новую последовательность, которую подразумевается сразу же начать исполнять, то для вызова начального колбека
+			// получим последний элемент вектора функцией .back(), которая и будет нашей вносимой
+			
+			if(sequences.back().startFunc)
+			{	sequences.back().startFunc(sequences.back().data);	}		// вызов стартовой функции!!!
+			
+			if(sequences.back().start_callback)
+			{ sequences.back().start_callback();	}
+			
+		}
+
+		void Execute() 										// функция Execute() должна вызываться из основной части программы. Например, можно вызывать ее в каждом цикле главной петли
+		{	
+			if( !sequences.empty() )
 			{
-				if(it->function)
-				{
-					bool result = it->function(it->data);
+				auto it = sequences.begin();
 				
-					if (result) 
+				while (it != sequences.end()) 
+				{
+					if(it->function)
 					{
-						if (it->endFunc) 		it->endFunc(it->data);		
+						bool result = it->function(it->data);
+					
+						if (result) 
+						{
+							if (it->endFunc) 		it->endFunc(it->data);		
+							if (it->end_callback) 	it->end_callback();
+							
+							it = sequences.erase(it); // erase returns next iterator
+						} 
+						else 
+						{
+							++it;
+						}
+					}
+					else // если функция вдруг оказалась пустой
+					{
+						//std::cout << "\t\tEmpty main func!\n";
+						
+						if (it->endFunc) 		it->endFunc(it->data);
 						if (it->end_callback) 	it->end_callback();
 						
-						it = sequences.erase(it); // erase возвращает следующий итератор
-					} 
-					else 
-					{
-						++it;
+						it = sequences.erase(it); // erase returns next iterator
 					}
-				}
-				else // если функция вдруг оказалась пустой
-				{
-					//std::cout << "\t\tEmpty main func!\n";
 					
-					if (it->endFunc) 		it->endFunc(it->data);
-					if (it->end_callback) 	it->end_callback();
-					
-					it = sequences.erase(it); // erase возвращает следующий итератор
 				}
-				
 			}
 		}
-	}
 
-	~biCycleSequencer()
-	{
-		sequences.clear(); //очистка вектора последовательностей
-	}
-};
+		
+	};
+	
+}
+
 
 
 #endif
--- biCycleExample.cpp
+++ biCycleExample.cpp
@@ -1,96 +1,76 @@
 /*
-	2025-03-04  indoostrialniy  <pawel.iv.2016@yandex.ru>
+	2025-04-15  indoostrialniy  <pawel.iv.2016@yandex.ru>
  
- 	Пример использования библиотеки biCycle v1.4
+ 	Пример использования библиотеки biCycle v1.5
  
 	Функция main() содержит цикл while, имитирующий основной цикл.
 	
 	Сторожевой таймер watchDog прерывает выполнение основного цикла на 100-й итерации.
 	
-	Класс Body содержит метод последовательности bool testFunc(BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE& data), которую можно подмешать в основной цикл. 
+	Класс Body содержит метод последовательности bool testFunc(), которую можно подмешать в основной цикл. 
 		Эта последовательность вызывает однократно колбек начала, отсчитывает свои собственные 30 раз выполнения с помощью внутреннего таймера timer и 
 		ВОЗВРАЩАЕТ true, позволяя тем самым Секвенсору прекратить её выполнение и вызвать колбек конца.
 
 	В теле функции main() по указателю body создается новый экземпляр класса Body, 
-	создается и инициализируется как false булева переменная completeFlag для отслеживания статуса завершенности подмешиваемой последовательности,
 	создается и инициализируется сторожевой таймер watchDog и вспомогательная переменная mixStep для указания момента,
 	когда можно	подмешать последовательность в основной цикл.
 	
 	Программа выводит в консоль соответствующие сообщения для объяснения логики работы.
-	В данном примере подмешивание одной и той же последовательности testFunc(BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE data) происходит дважды - на 5-м и 50-м цикле.
+	В данном примере подмешивание одной и той же последовательности testFunc() происходит единожды.
 */
 
 #include "biCycle.h"
 
-struct myData	// собственная структура данных позволит при дальнейшем масштабировании не перелопачивать весь код, добавляя аргументы. Т.о - задел на будущее
-{	
-	myData( bool* bReportPtr = nullptr, int value = 0 ) : report(bReportPtr), var(value) {}
-	
-	int timer = 0;				// таймер для отсчета времени выполнения последовательности
-	
-	bool* 	report 	= nullptr; 	//указатель на булев флаг завершенности действия (для отчета, не обязателен)
-	int 	var 	= 0;		// вспомогательная переменная
-};
 
-
-#define BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE myData
-
-
-
-biCycleSequencer<BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE> Flow;	// создали экземпляр с заданным типом данных
+biCycle::Sequencer<bool> Flow;					// создали экземпляр без заданного типа данных
 
 
 
 class Body										// класс-пример
 {
 public:
-	bool testFunc(BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE& data)	// эта функция может вызываться Секвенсором в каждом цикле, будучи однажды подмешанной в него. По-умолчанию var = 0 !
+	bool testFunc()	// эта функция может вызываться Секвенсором в каждом цикле, будучи однажды подмешанной в него. По-умолчанию var = 0 !
 	{		
-		if(data.timer == 30)		// условие завершения последовательности
+		if(timer == 30)		// условие завершения последовательности
 		{
 			std::cout << "\n\t\t\tMixed func has complete IT`S OWN INDEPENDENT(!) 30 cycles and set flag to True\n";
-			data.timer = 0; // обнуляем таймер для обеспечения возможности повторного запуска последовательности
+			timer = 0; // обнуляем таймер для обеспечения возможности повторного запуска последовательности
 			return true;	
 		}
 		else 						// последовательность еще выполняется
 		{
-			std::cout << "\n\t\t\tMixed stream cycle: " << data.timer;
+			std::cout << "\n\t\t\tMixed stream cycle: " << timer;
 			//if(var != 0) {std::cout << "\tAlso have an aux int variable: " << var;}
-			data.timer++;
+			timer++;
 			return false;	
 		}
+		//return true;
 	}
 	
+	int timer = 0;
 	
-	bool custom_start(BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE& data)
+	bool custom_start()
 	{
-		std::cout << "\n\t\tCustom start. timer=50\n";
-		data.timer += 10;
+		std::cout << "\t\t\tCustom start.\n";
 		return true;
 	}
 	
-	bool custom_end(BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE& data)
+	bool custom_end()
 	{
-		std::cout << "\n\t\tCustom end, yohohohoh, its ready!!\n";
-		*(data.report) = !*(data.report);
+		std::cout << "\t\t\tCustom end.\n";
 		return true;
 	}
 	
 	
-	void load(bool& completeFlag, int auxVar = 0)
+	void load()
 	{
-		// 1. функция принимает некие ссылочные аргументы completeFlag и auxVar, заносит их в пользовательскую структуру datas		
-		BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE datas(&completeFlag, auxVar);
-		
-		
-		// 2. затем формирует структуру для описания заносимой в секвенсор последовательности:
-		biCycleSequenceWrapper<BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE> sequenceParameters;		// структура со списком инициализирующих параметров по умолчанию
-			sequenceParameters.function = std::bind( &Body::testFunc, this, datas);				// укажем необходимое
-			sequenceParameters.startFunc = std::bind( &Body::custom_start, this, datas);
-			sequenceParameters.endFunc = std::bind( &Body::custom_end, this, datas);
-			sequenceParameters.data = datas;
-		
-		// 3. занесем сконфигурированную последовательность в секвенсор
+		// 1.
+		biCycle::Wrapper<bool> sequenceParameters;
+			sequenceParameters.function = std::bind( &Body::testFunc, this);
+			sequenceParameters.start_callback = std::bind( &Body::custom_start, this);	// по желанию
+			sequenceParameters.end_callback = std::bind( &Body::custom_end, this);	// по желанию
+			
+		// 2. занесем сконфигурированную последовательность в секвенсор
 		Flow.CallSequence( std::move(sequenceParameters) );										
 	}
 	
@@ -107,16 +87,10 @@ int main()
 	
 	Body* body = new Body;		// создаем новый экземпляр класса Body
 	
-	bool completeFlag = false;	// переменная для хранения сигнала о завершении выполнения последовательности
-	
-
-	
 	int watchDog = 0;			// счетчик сторожевого таймера
 	
 	int mixStep = 5;			// вспомогательная переменная для данного примера для указания, в какой момент времени подмешать функцию в поток
-	
-	int& auxVar = mixStep;		// ссылочная переменная-копия, которую передаем в подмешиваемую последовательность в качестве аргумента наряду с адресом экземпляра класса-владельца. Можно указать любое требуемое число
-	
+		
 	while(1)
 	{
 		std::cout << "Main stream cycle: " << watchDog;
@@ -124,17 +98,12 @@ int main()
 		// последовательность можно подмешать в основной цикл в любой момент. Для примера, сделаем это сначала на 5 итерации
 		if(watchDog == mixStep)	
 		{
-			std::cout << "\nSend sequence testFunc(BICYCLE_LIBRARY_SEQUENCES_ARGUMENT_TYPE& data) to execution at cycle: " << mixStep << "\n";
+			std::cout << "\nSend sequence testFunc() to execution at cycle: " << mixStep << "\n";
 	
-			body->load(completeFlag, 0);
+			body->load();
 		}
 		
-		if(completeFlag)	// когда отчетный буль стал true
-		{
-			std::cout << "\nMain stream catched that mixed func returns TRUE! Set mixStep = 50.\n";
-			mixStep = 50;
-			completeFlag = false;	// так как при явном указании булева флага о статусе выполнения функции по ее завершении он ИНВЕРТИРУЕТСЯ, то для повторного запуска необходимо заново установить его в false!
-		}
+		
 		
 		
 		if(watchDog == 100)
